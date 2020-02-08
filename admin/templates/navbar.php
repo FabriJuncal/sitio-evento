78b382abf7e9358f98a01c0e1960782e94b538d2
Fabricio Juncal <juncalfabri@gmail.com>
2020-02-08 18:49:32 -0300

    Iniciando el Panel de Administracion

---
+++ admin/templates/navbar.php
@@ -0,0 +1,37 @@
+<body class="hold-transition sidebar-mini">
+<!-- Site wrapper -->
+<div class="wrapper">
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
+      </li>   
+    </ul>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+            <!-- Notifications Dropdown Menu -->
+            <li class="nav-item dropdown">
+            <a class="nav-link" data-toggle="dropdown" href="#">
+              <i class="fas fa-users mr-2"></i> Admin
+            </a>
+            <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+              
+              <div class="dropdown-divider"></div>
+              <a href="#" class="dropdown-item">
+                <i class="fas fa-cogs mr-2"></i></i> Configuración
+              </a>
+              <div class="dropdown-divider"></div>
+              <a href="#" class="dropdown-item">
+                <i class="fas fa-sign-out-alt mr-2"></i> Salir
+              </a>
+              
+            </div>
+          </li>
+    </ul>
+
+
+  </nav>
+  <!-- /.navbar -->
